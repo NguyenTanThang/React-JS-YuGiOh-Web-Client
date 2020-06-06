552eb505bb140583532c489e79725dbdce2b944b
Victor <nguyentanthang9a4@gmail.com>
2020-06-06 14:17:11 +0700

    Version 1.1.1 - Add remove card from deck feature

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-//export const MAIN_PROXY_URL = "https://yugioh-server.herokuapp.com";
-export const MAIN_PROXY_URL = "http://localhost:5000";
+export const MAIN_PROXY_URL = "https://yugioh-server.herokuapp.com";
+//export const MAIN_PROXY_URL = "http://localhost:5000";

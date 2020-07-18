e18d1f6e3ccff91d05cb28bb4e1b2360323b8c07
Victor <nguyentanthang9a4@gmail.com>
2020-07-18 15:49:00 +0700

    Version 1.1.2 - Get rid of tags in description and add in category manipulation ability

--- src/config/store.js
+++ src/config/store.js
@@ -3,9 +3,8 @@ import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
 
 const store = createStore(rootReducer, {}, compose(
-    applyMiddleware(
-    thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    applyMiddleware(thunk))
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 export default store;
